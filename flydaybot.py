c23fbc25326ade935481ab646b691e0b102f8e67
Lawton Skaling <lskaling@icloud.com>
2023-09-20 01:36:49 -0800

    Test

--- flydaybot.py
+++ flydaybot.py
@@ -55,8 +55,22 @@ def get_flight_coordinators():
 
 def upload_ics_content_to_google_drive(ics_content, folder_id):
     # Set up Google Drive API credentials
-    credentials = service_account.Credentials.from_service_account_file(
-        'fly-day-bot-calendar-upload-63b752d25530.json',  # Replace with your credentials JSON file path
+    credentials_info = {
+        "type": "service_account",
+        "project_id": os.environ.get("PROJECT_ID"),
+        "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("PRIVATE_KEY"),
+        "client_email": os.environ.get("CLIENT_EMAIL"),
+        "client_id": os.environ.get("CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ.get("CLIENT_X509"),
+        "universal_domain": "googleapis.com"
+    }
+
+    credentials = service_account.Credentials.from_service_account_info(
+        credentials_info,
         scopes=['https://www.googleapis.com/auth/drive']
     )
 
